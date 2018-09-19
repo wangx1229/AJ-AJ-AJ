81bb885278288696de05a2c481c6b67c708b4c97
Shaoshuai Dong 董少帅 <shaoshuai.dong@nio.com>
2018-09-19 13:48:47 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // 扫描间隔
+    CheckInterval: 2000,
+    // 邮箱服务器设置
+    EmailConf: {
+        host: 'smtp.qq.com',
+        secureConnection: true,
+        auth: {
+            user: '1570680400@qq.com',
+            pass: 'sejlrpjgzxyqggjf'
+        }
+    },
+    // 发件人
+    EmailFrom: '1570680400@qq.com',
+    // 收件箱
+    EmailTo: [
+        '1172830949@qq.com',
+        '1570680400@qq.com'
+    ]
+}
